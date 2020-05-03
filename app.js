18fd2ab387ebe41a4cb6d2d31106da6df9f85d89
szolzol <szolzol@users.noreply.github.com>
2020-05-03 16:01:44 +0200

    secrets

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 //jshint esversion: 6;
 
-// API key: a17e2286b51c064b1067af03e1385833-us8
-// audience ID: 4a8b6fdb8f
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const https = require('https')
 
 const app = express();
+
+const keys = require('./keys.js');
 
 app.set('port', process.env.PORT || 3000);
 
@@ -46,14 +45,13 @@ app.post('/', (req, res) => {
 
     const jsonData = JSON.stringify(data);
 
-    const url = "https://us8.api.mailchimp.com/3.0/lists/4a8b6fdb8f";
 
     const options = {
         method: "POST",
-        auth: "szolzol:a17e2286b51c064b1067af03e1385833-us8"
+        auth: keys.auth
     }
 
-    const request = https.request(url, options, function (response) {
+    const request = https.request(keys.url, options, function (response) {
         
         if (response.statusCode === 200) {
             res.sendFile(__dirname + "/success.html");
